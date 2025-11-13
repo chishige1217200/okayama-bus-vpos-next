f7aba8b5fae7e8ce3c28702c5a95b8e10c5f60cd
chishige1217200 <daichicchi01@gmail.com>
2025-11-14 00:38:51 +0900

    Add: 岡電バス用APIを暫定作成

---
+++ app/api/okaden/trip_update/route.ts
@@ -0,0 +1,32 @@
+import { fetchData } from "@/lib/busVisionApi";
+import { ApiErrorResponse } from "@/types/ApiErrorResponse";
+import { TripUpdate } from "@/types/tripUpdate";
+import { NextResponse } from "next/server";
+
+// GET /api/okaden/trip_update/
+export async function GET(): Promise<
+  NextResponse<TripUpdate[] | ApiErrorResponse>
+> {
+  try {
+    return NextResponse.json(
+      await fetchData(
+        "https://loc.bus-vision.jp/realtime/okaden_trip_update.bin"
+      ),
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      const apiErrorResponse: ApiErrorResponse = {
+        error: error.message,
+        status: 500,
+      };
+      return NextResponse.json(apiErrorResponse, { status: 500 });
+    } else {
+      const apiErrorResponse: ApiErrorResponse = {
+        error: "Unknown error occurred",
+        status: 500,
+      };
+      return NextResponse.json(apiErrorResponse, { status: 500 });
+    }
+  }
+}
