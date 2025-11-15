edee43f9e453ffc019e063d433155c8c07ef91a4
chishige1217200 <daichicchi01@gmail.com>
2025-11-15 16:06:34 +0900

    Add: 静的データを返す型を定義

---
+++ types/gtfsFeed.ts
@@ -0,0 +1,12 @@
+export interface Routes {
+  route_id: string;
+  agency_id: string;
+  route_short_name: string;
+  route_long_name: string;
+  route_desc: string;
+  route_type: string;
+  route_url: string;
+  route_color: string;
+  route_text_color: string;
+  jp_parent_route_id: string;
+}
---
+++ types/icon.ts
@@ -0,0 +1,4 @@
+export interface Icon {
+  label: string;
+  url: string;
+}
