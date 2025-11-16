0bb5b86d59dfe9cb13ea2463d958a43f42546291
chishige1217200 <daichicchi01@gmail.com>
2025-11-17 00:32:20 +0900

    Add: 新システム画面のモック作成

---
+++ components/main.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import "./main.css";
+import { UserLocation } from "@/types/userLocation";
+import { GoogleMap, LoadScript, OverlayView } from "@react-google-maps/api";
+import Image from "next/image";
+
+const Main = () => {
+  // const [markers, setMarkers] = useState([]);
+  const [activeMarkerId, setActiveMarkerId] = useState(null); // 開いているInfoWindowFを追跡
+  // const [isLoading, setIsLoading] = useState(true); // ローディング状態
+  const [center, setCenter] = useState({ lat: 34.663, lng: 133.925 }); // 初期値
+  const [userLocation, setUserLocation] = useState<UserLocation | null>(null); // ユーザーの現在地
+
+  useEffect(() => {
+    console.log("Fetching user location...");
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setCenter({ lat: latitude, lng: longitude }); // 現在地を中心に設定
+          setUserLocation({ lat: latitude, lng: longitude }); // 現在地を保存
+        },
+        (error) => {
+          console.error("Error getting location:", error);
+        }
+      );
+    } else {
+      console.warn("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
+  return (
+    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_API_KEY ?? ""}>
+      <GoogleMap
+        mapContainerStyle={{ height: "100vh", width: "100vw" }}
+        center={center}
+        zoom={17}
+      >
+        {userLocation && (
+          <OverlayView
+            position={userLocation}
+            mapPaneName={OverlayView.OVERLAY_MOUSE_TARGET}
+          >
+            <div className="pulse-container">
+              <Image
+                src="/bluedot.png"
+                width={1}
+                height={1}
+                alt="現在地"
+                className="pulse-dot"
+              />
+              <div className="pulse-ring"></div>
+            </div>
+          </OverlayView>
+        )}
+        {/* {markers.map((marker) => (
+            <React.Fragment key={marker.id}>
+            </React.Fragment>
+          ))} */}
+      </GoogleMap>
+    </LoadScript>
+  );
+};
+
+export default Main;
