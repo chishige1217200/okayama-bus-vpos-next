eca375254e5cc2a3bdbca66f08c7f5734248bdbe
chishige1217200 <daichicchi01@gmail.com>
2026-05-16 19:47:39 +0900

    Add: 更新ボタンを最大化表示時も表示するように対応

--- components/main.tsx
+++ components/main.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./main.css";
+import { Maximize, RotateCw } from "lucide-react";
 import { GoogleMap, LoadScript, OverlayView } from "@react-google-maps/api";
 import Image from "next/image";
 import MarkerGroup from "./marker";
 import { useFetchController } from "@/context/FetchContext";
 import { useAgency } from "@/context/AgencyContext";
-import { Button } from "@chakra-ui/react/button";
+import { IconButton } from "@chakra-ui/react/button";
 import { Theme } from "@chakra-ui/react";
 
 const Main = () => {
@@ -19,6 +20,30 @@ const Main = () => {
   }); // 初期値
   const [userLocation, setUserLocation] =
     useState<google.maps.LatLngLiteral | null>(null); // ユーザーの現在地
+
+  const mapWrapperRef = useRef<HTMLDivElement>(null);
+
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  useEffect(() => {
+    const handleFullscreenChange = () => {
+      setIsFullscreen(!!document.fullscreenElement);
+    };
+
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
+
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullscreenChange);
+    };
+  }, []);
+
+  const toggleFullscreen = async () => {
+    if (!document.fullscreenElement) {
+      await mapWrapperRef.current?.requestFullscreen();
+    } else {
+      await document.exitFullscreen();
+    }
+  };
 
   useEffect(() => {
     console.log("Fetching user location...");
@@ -44,23 +69,30 @@ const Main = () => {
         <LoadScript
           googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY ?? ""}
         >
-          <div className="relative">
+          <div
+            ref={mapWrapperRef}
+            className="relative"
+            style={{
+              width: "100vw",
+              height: "100vh",
+            }}
+          >
+            {/* ボタン群 */}
             <div
               style={{
                 position: "absolute",
                 top: 10,
-                right: 60,
+                right: 10,
                 display: "flex",
+                gap: "8px",
                 zIndex: 5,
                 color: "white",
               }}
             >
-              <Button
+              {/* 更新ボタン */}
+              <IconButton
                 onClick={fire}
                 disabled={isLoading}
-                position="absolute"
-                top="0"
-                right="0"
                 h="40px"
                 w="40px"
                 bg="white"
@@ -72,16 +104,39 @@ const Main = () => {
                 cursor="pointer"
                 userSelect="none"
                 textTransform="none"
-                _hover={{ bg: "white" }} // Google風はhoverでほぼ変化なし
+                _hover={{ bg: "white" }}
+                _active={{ bg: "gray.100" }}
+                loading={isLoading}
+              >
+                <RotateCw />
+              </IconButton>
+
+              {/* 最大化ボタン */}
+              <IconButton
+                onClick={toggleFullscreen}
+                h="40px"
+                w="40px"
+                bg="white"
+                color="black"
+                border="0"
+                borderRadius="2px"
+                boxShadow="0px 1px 4px -1px rgba(0, 0, 0, 0.3)"
+                _hover={{ bg: "white" }}
                 _active={{ bg: "gray.100" }}
               >
-                更新
-              </Button>
+                <Maximize />
+              </IconButton>
             </div>
             <GoogleMap
-              mapContainerStyle={{ height: "100vh", width: "100vw" }}
+              mapContainerStyle={{
+                height: "100%",
+                width: "100%",
+              }}
               center={center}
               zoom={17}
+              options={{
+                fullscreenControl: false,
+              }}
             >
               {userLocation && (
                 <OverlayView
